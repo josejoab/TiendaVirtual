b603c2e7831be26d3f3082ce66122fbcf485a377
KevinHerrera <kaherrerag@eafit.edu.co>
2020-09-12 21:35:37 -0500

    Models and Migration Category and design

---
+++ DesignVirtualStore/database/migrations/2020_09_12_000000_create_design_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDesignTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->integer('price');
+            $table->text('description');
+            $table->text('image');
+            $table->integer('width');
+            $table->integer('length');
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('designs');
+    }
+}
