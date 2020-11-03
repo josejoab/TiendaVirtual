b2458dee4b870253bbd027f64328bdf43a3489a7
Joab Romero <jjromeroh@eafit.edu.co>
2020-11-03 12:25:13 -0500

    servicio de json agregado

--- DesignVirtualStore/routes/api.php
+++ DesignVirtualStore/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/designs', 'Api\DesignApi@index')->name("api.design.index");
+Route::get('/designs/paginate', 'Api\DesignApi@paginate')->name("api.design.paginate");
