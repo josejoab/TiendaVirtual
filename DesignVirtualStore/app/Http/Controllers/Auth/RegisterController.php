b6612f8850e6177803b9f38c4673f9778794e98f
josejoab <41337937+josejoab@users.noreply.github.com>
2020-11-11 00:14:21 -0500

    Update RegisterController.php

--- DesignVirtualStore/app/Http/Controllers/Auth/RegisterController.php
+++ DesignVirtualStore/app/Http/Controllers/Auth/RegisterController.php
@@ -34,7 +34,7 @@ class RegisterController extends Controller
      * @var string
      */
 
-    protected $redirectTo = 'public/en/user';
+    protected $redirectTo = '/en/user';
     //protected $redirectTo = '/en/user';
 
     /**
